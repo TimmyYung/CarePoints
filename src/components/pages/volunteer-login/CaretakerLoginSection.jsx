38d127ab62bea12bb99a330845bc79c78e154202
Wendy Shen <wendyshen2005@gmail.com>
2024-11-10 04:16:17 -0500

    osudhsadf

--- src/components/pages/volunteer-login/CaretakerLoginSection.jsx
+++ src/components/pages/volunteer-login/CaretakerLoginSection.jsx
@@ -6,31 +6,21 @@ import { useState } from "react";
 
 const CaretakerLoginSection = () => {
 
-    const [emailError, setEmailError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleLogin = () => {
-        if(document.getElementById("volunteer-email").innerHTML == "")
-            setEmailError(true);
-        else
-            setEmailError(false);
-
-        if(document.getElementById("volunteer-password").innerHTML == "")
-            setPasswordError(true);
-        else
-            setPasswordError(false);
-
-        if(!emailError && !passwordError)
-            window.location = "../volunteer/find-clients";
+        if(email != "" && password != "")
+            window.location = "../volunteer/find-clients"
     }
     return (
         <div id="caretaker-login-section">
             <div id="caretaker-login">
                 <h3>Volunteer Login</h3>
                 <br/>
-                <TextField error={emailError} required label="Email:" id="volunteer-email" onChange={(e) => {if(e.target.value != "") setEmailError(false)}}/>
+                <TextField error={email == ""} required label="Email:" id="volunteer-email" onChange={(e) => {setEmail(e.target.value)}}/>
                 <br/>
-                <TextField error={passwordError} required label="Password:" id="volunteer-password" onChange={(e) => {if(e.target.value != "") setPasswordError(false)}}/>
+                <TextField error={password == ""} required label="Password:" id="volunteer-password" onChange={(e) => {setPassword(e.target.value)}}/>
                 <br/>
                 <a href="../volunteer/sign-up" style={{color:"blue"}}>Don't have an account?</a><br/>
                 <Button onClick={handleLogin}>Login</Button>
