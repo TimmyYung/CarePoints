9fc7f9ba7b218f3eef2aaa7fd08950b3d6bf61f8
Wendy Shen <wendyshen2005@gmail.com>
2024-11-09 23:30:31 -0500

    started rest of caretaker pages

---
+++ src/app/(routes)/caretaker-edit-profile/page.jsx
@@ -0,0 +1,17 @@
+
+// import { Metadata } from "next";
+
+import CaretakerEditProfile from "@/components/pages/caretaker-edit-profile/CaretakerEditProfile";
+
+// export const metadata: Metadata = {
+//   title: "Achievements",
+//   description: "Page to earn and view achievements",
+// };
+
+const homePage = () => {
+    return (
+        <CaretakerEditProfile/>
+    );
+  };
+
+export default homePage;
