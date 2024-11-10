c88739c08541db872558455bf3a86ff703c46aa5
Wendy Shen <wendyshen2005@gmail.com>
2024-11-10 09:24:04 -0500

    style changes

--- src/components/pages/volunteer-home-pages/VolunteerApps.jsx
+++ src/components/pages/volunteer-home-pages/VolunteerApps.jsx
@@ -4,19 +4,19 @@ const CaretakerApps = ({data}) => {
     return (
         <div className="volunteer-application">
             <div style={{display:"flex", justifyContent:"space-between"}}>
-                <h3>{data.clientEmail}</h3>
-                <h3>Points: {data.points}</h3>
+                <h3 style={{fontWeight:"normal"}}>{data.clientEmail}</h3>
+                <h3 style={{fontWeight:"normal"}}>Points: {data.points}</h3>
             </div>
-            <p><span className="bold">Postal Code:</span> {data.postal}</p><br/>
+            <p>Postal Code: {data.postal}</p><br/>
 
             <div className="volunteer-app-categories">Categories:{data.categories.map((cat, i) => {
-                return (<button key={i} disabled>{cat}</button>)
+                return (<button key={i} disabled className="volunteer-find-client-button">{cat}</button>)
             })}</div>
             <br/>
-            <div><span className="bold">Volunteers Needed: </span> {data.volunteersNeeded}</div>
+            <div>Volunteers Needed: {data.volunteersNeeded}</div>
             <br/>
-            <div><span className="bold">Notes: </span> {data.notes}</div>
-            <Button>Apply</Button>
+            <div>Notes: {data.notes}</div>
+            <Button style={{fontFamily:"pixel", background:"rgb(151, 174, 242)", border:"1px solid blue", borderRadius:"0"}}>Apply</Button>
         </div>
     )
 }
